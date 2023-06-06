af862e31cc0930883998c8dc9885e57e24958047
Ítalo Penha <italopenha77@outlook.com>
2023-06-06 11:38:30 -0300

    Exemplos de serviços

---
+++ src/app/exemplo-servico2/exemplo-servico2.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { LoggerService } from '../logger.service';
+
+@Component({
+  selector: 'app-exemplo-servico2',
+  templateUrl: './exemplo-servico2.component.html',
+  styleUrls: ['./exemplo-servico2.component.css']
+})
+export class ExemploServico2Component {
+  descricao = "";
+
+  constructor(public logger: LoggerService) {}
+
+  adicionarProduto() {
+    this.logger.logar(`O produto com a descrição ${this.descricao} foi adicionado`);
+  }
+}
